df358557d1b1f4ade172e9c016a3e87f59cc328a
Ezra Szanton <54345209+Delverino@users.noreply.github.com>
2022-11-11 13:07:44 -0500

    rough draft of direction code (may not be useful yet)

--- Assets/Scripts/Directions.cs
+++ Assets/Scripts/Directions.cs
@@ -1,18 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Directions : MonoBehaviour
+public enum Dir{
+    Up, Down, Left, Right
+}
+
+public class Direction
 {
-    // Start is called before the first frame update
-    void Start()
+    private static float EPSILON = 0.001f;
+    Direction(int _x, int _y)
     {
-        
+        x = _x;
+        y = _y;
     }
 
-    // Update is called once per frame
-    void Update()
+    Direction(Vector2Int d)
     {
-        
+        x = d.x;
+        y = d.y;
     }
+
+    Direction(Vector2 d)
+    {
+        x = y = 0;
+        if (d.x > EPSILON)
+        {
+            x = 1;
+        } else if (d.x < -EPSILON)
+        {
+            x = -1;
+        }
+        
+        if (d.y > EPSILON)
+        {
+            y = 1;
+        } else if (d.y < -EPSILON)
+        {
+            y = -1;
+        }
+
+        if (x != 0 && y != 0)
+        {
+            if (Mathf.Abs(d.x) < Mathf.Abs(d.y))
+            {
+                x = 0;
+            }
+            else
+            {
+                y = 0;
+            }
+        }
+    }
+
+    Direction(Dir d)
+    {
+        if (d == Dir.Up)
+        {
+            x = 0;
+            y = 1;
+        } else if (d == Dir.Down)
+        {
+            x = 0;
+            y = -1;
+        } else if (d == Dir.Left)
+        {
+            x = -1;
+            y = 0;
+        } else if (d == Dir.Right)
+        {
+            x = 1;
+            y = 0;
+        }
+    }
+    private int x, y;
+    public int X => x;
+    public int Y => y;
+}
+
+public class Directions
+{
+    
 }
