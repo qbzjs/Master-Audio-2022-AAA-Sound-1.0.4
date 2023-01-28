226c55b7060d1fd2e801b65d0c590c8fc6e95acd
Ezra Szanton <54345209+Delverino@users.noreply.github.com>
2023-01-28 16:56:29 -0500

    Working ghosts and humans and vampires

---
+++ Assets/Scripts/Tiles/Ghost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scripts;
+using UnityEngine;
+
+public class Ghost : Wasteland
+{
+    [SerializeField] new protected int scoreWorth = 10;
+    
+    public Ghost(Transform parentTransform, Vector3 pos) : base(parentTransform, pos)
+    {
+
+    }
+    protected override Score CalculateBaseScore()
+    {
+        return new Score(scoreWorth);
+    }
+    
+}
