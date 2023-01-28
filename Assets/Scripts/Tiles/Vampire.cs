b7d6dd2e53b27a89308bb17a3b20d53916563a48
Ezra Szanton <54345209+Delverino@users.noreply.github.com>
2023-01-28 16:56:29 -0500

    Working ghosts and humans and vampires

---
+++ Assets/Scripts/Tiles/Vampire.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scripts;
+using UnityEngine;
+
+public class Vampire : Wasteland
+{
+    [SerializeField] new protected int scoreWorth = 4;
+
+    private int timesFeasted = 0;
+    
+    public Vampire(Transform parentTransform, Vector3 pos) : base(parentTransform, pos)
+    {
+
+    }
+
+    public override void Observe(DefaultEvent e)
+    {
+        if (xPos != e.xPos || yPos != e.yPos)
+        {
+            //If we're not referencing this object, don't do anything
+            return;
+        }
+
+        foreach (Vector2Int dir in Directions.Cardinal)
+        {
+            Vector2Int lookingAt = new Vector2Int(xPos + dir.x, yPos + dir.y);
+            ITile potentialKill = GridManager.Instance.GetTile(lookingAt.x, lookingAt.y);
+            if (potentialKill is Human)
+            {
+                GridManager.Instance.DestroyTile(lookingAt);
+                timesFeasted++;
+            }
+        }
+    }
+
+    protected override Score CalculateBaseScore()
+    {
+        return new Score(4 + timesFeasted * 3, $"4 + [{timesFeasted}] * 3");
+    }
+    
+}
