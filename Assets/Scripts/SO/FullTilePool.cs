0516deeb6c56ea7974a11d7fe73df896da34b9a9
Ezra Szanton <ezraszanton@yahoo.com>
2023-02-07 12:03:05 -0500

    Changed starting deck to be stored with the rest of the tile rarities

--- Assets/Scripts/SO/FullTilePool.cs
+++ Assets/Scripts/SO/FullTilePool.cs
@@ -2,12 +2,14 @@ using System;using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using RotaryHeart.Lib.SerializableDictionary;
 
 [CreateAssetMenu(menuName = "Tile Pool")]
 public class FullTilePool : ScriptableObject
 {
      [BoxGroup("Rarity Weights - must add to 1")] public float CommonWeight, UncommonWeight, RareWeight, LegendaryWeight;
-     [BoxGroup("Rarity Colors")] public Color CommonColor, UncommonColor, RareColor, LegendaryColor;
+     [BoxGroup("Rarity Colors")] public Color CommonColor, UncommonColor, RareColor, LegendaryColor;     
+     [BoxGroup("Tile Lists"), SerializeField] public SerializableDictionaryBase<string, int> startingDeck;
      [BoxGroup("Tile Lists")] public List<string> Commons, Uncommons, Rares, Legendaries;
 
 
