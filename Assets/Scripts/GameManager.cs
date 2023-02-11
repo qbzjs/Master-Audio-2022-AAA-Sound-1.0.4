3b6e95b9d991612e098300d8957a12bf2f689daa
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2023-02-11 16:47:21 -0500

    Revert "tile interaction- in progress"
    This reverts commit 99242a69fbec74ddb088fb21fd3dcfdca22364ae.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,8 +23,6 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private GameObject winButton, winScreen, loseScreen, upgradeScreen;
     [Foldout("UI")]
     [SerializeField] public Tooltip tooltip;
-    [Foldout("UI")]
-    [SerializeField] public ScoreTip scoretip;
     [SerializeField] private FullTilePool tilePool;
 
     [SerializeField, BoxGroup("Difficulty Parameters")] private int winningScore, upgradeIncrement, totalTurns, winningScoreIncrement;
