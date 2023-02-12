f6dcadbc04c4366b4a30d26f166cb66425726978
Ezra Szanton <ezraszanton@yahoo.com>
2023-02-12 17:15:58 -0500

    Added rat king

---
+++ Assets/Scripts/Tiles/Creatures/RatKing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scripts;
+using UnityEngine;
+
+public class RatKing : Rat
+{
+    [SerializeField] new protected int scoreWorth = 0;
+
+    public override string GetDescription()
+    {
+        return $"COMBINES ALL RATS";
+    }
+    
+    public RatKing(Transform parentTransform, Vector3 pos) : base(parentTransform, pos)
+    {
+        
+    }
+
+    protected override Score CalculateBaseScore()
+    {
+        return new Score(scoreWorth);
+    }
+
+    public override void WhenPlaced()
+    {
+        GridManager.ForEach((int x, int y, Rat rat) =>
+        {
+            if (x == xPos && y == yPos) return; //if it's me, return
+
+            scoreWorth += rat.CalculateScore().score;
+            GridManager.Instance.DestroyTile(new Vector2Int(x, y));            
+        });
+    }
+}
