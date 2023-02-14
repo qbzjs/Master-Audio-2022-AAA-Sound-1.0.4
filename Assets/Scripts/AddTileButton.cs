f049faf0996a62902a0a8c264d247fca56b10fac
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2023-02-14 17:18:23 -0500

    deck layout

--- Assets/Scripts/AddTileButton.cs
+++ Assets/Scripts/AddTileButton.cs
@@ -11,9 +11,13 @@ public class AddTileButton : MonoBehaviour
     public Card card;
     private string tileName;
 
-    public void SetValues(string myTileName)
+    public void SetValues(string myTileName, Color newColor)
     {
         tileName = myTileName;
+        Image cardImage = card.GetComponent<Image>();
+        cardImage.color = newColor;
+        Image tileImage = card.transform.GetChild(0).gameObject.GetComponent<Image>();
+        tileImage.color = newColor;
         card.CreateCardNewTile(myTileName);
     }
 
