c6aae1b2f7cfb78de1e88fc6d85ff41c4345da08
Ezra Szanton <54345209+Delverino@users.noreply.github.com>
2023-03-09 00:33:40 -0500

    Callout popups

--- Assets/Scripts/Tiles/Buildings/River.cs
+++ Assets/Scripts/Tiles/Buildings/River.cs
@@ -49,6 +49,7 @@ public class River : Building
             if (tile.GetTags().Contains(Tag.Blood))
             {
                 //Debug.Log("going to turn: " + tile);
+                TweenManager.Instance.Callout("Blood River Flows!", Position());
                 GridManager.Instance.PlaceTile("BloodRiver", new Vector2Int(xPos, yPos));
                 return true;
             }
