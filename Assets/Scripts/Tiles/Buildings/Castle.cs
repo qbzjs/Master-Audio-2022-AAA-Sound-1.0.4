16b7b8f966e99a211e076e8d426a838a04cb121b
JakeSturim <40542949+JakeSturim@users.noreply.github.com>
2023-03-10 12:39:19 -0500

    please don't break

---
+++ Assets/Scripts/Tiles/Buildings/Castle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Scripts;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Castle : Building
+{
+    [SerializeField] protected int scoreWorthAdjacent = 2;
+
+    private List<Tag> newTags = new List<Tag>();
+
+    public override string GetDescription()
+    {
+        return "+2 point for each adjacent castle. If next to a monster, score is 6 but cannot be increased (eat the rich).";
+    }
+
+    public override Tag[] GetTags()
+    {
+        return newTags.ToArray();
+    }
+
+
+    public Castle(Transform parentTransform, Vector3 pos) : base(parentTransform, pos)
+    {
+
+    }
+
+    protected override Score CalculateBaseScore()
+    {
+        int adjacentCastles = 0;
+
+        bool adjacentMonster = false;
+
+        foreach (Vector2Int dir in Directions.Cardinal)
+        {
+            ITile tile = GridManager.Instance.GetTile(xPos + dir.x, yPos + dir.y);
+            if (tile is Castle)
+            {
+                adjacentCastles++;
+            }
+            if (tile.GetTags().Contains(Tag.Monster))
+            {
+                adjacentMonster = true;
+                foreach (Tag tag in tile.GetTags())
+                {
+                    if (!newTags.Contains(tag) && tag != Tag.Monster)
+                    {
+                        newTags.Add(tag);
+                    }
+                }
+            }
+        }
+
+        if (adjacentMonster)
+        {
+            return new Score(6);
+        }
+
+        return new Score(2 + adjacentCastles * scoreWorthAdjacent);
+    }
+}
---
+++ Assets/Scripts/Tiles/Creatures/Witch.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts;
+using System.Threading;
+using UnityEngine.SocialPlatforms.Impl;
+using NUnit.Framework.Internal;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+
+public class Witch : Monster
+{
+    [SerializeField] protected int scoreWorth = 2;
+
+    public override string GetDescription()
+    {
+        return "<i>Worth 2 points, in pack of 5 increases score of best tile on board.</i>";
+    }
+
+    public Witch(Transform parentTransform, Vector3 pos) : base(parentTransform, pos)
+    {
+        Type = "Chaos";
+    }
+
+    public override Tag[] GetTags()
+    {
+        return new[] { Tag.Chaos, Tag.Monster };
+    }
+
+
+    private static Effect WitchMultiplier =
+        new Effect(
+            "Witch multiplier", 20, 1, 5, (value) =>
+            {
+                return new Score(value.score + 10, value.explanation + " + 10");
+            }
+        );
+
+    private static Rule PackOfWitches = new Rule("Pack Of Witches", 9, () =>
+    {
+        ITile bestTile = null;
+        GridManager.ForEach((int x, int y, ITile tile) => {
+            if (bestTile == null)
+            {
+                bestTile = tile;
+            }
+            else
+            {
+                if (tile.CalculateScore().score > bestTile.CalculateScore().score)
+                {
+                    bestTile = tile;
+                }
+            }
+        });
+        bestTile.AddEffect(WitchMultiplier);
+    });
+
+    public override void WhenPlaced()
+    {
+        CalculatePack();
+    }
+
+    private void CalculatePack()
+    {
+        List<Creature> pack = new();
+        pack.Add(this);
+        int packCount = CountGroupCreatures(this.GetType(), pack);
+
+        if (packCount >= packSize)
+        {
+            GameManager.Instance.AddRule(PackOfWitches);
+        }
+    }
+
+    protected override Score CalculateBaseScore()
+    {
+        return new Score(scoreWorth);
+    }
+}
